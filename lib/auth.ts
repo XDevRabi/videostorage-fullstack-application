e2e4e58ba70eb8a8c2311a110ba02c5cb103fef3
Rabi Shiwakoti <46970640+XDevRabi@users.noreply.github.com>
2025-08-07 16:25:56 +0545

    fix(auth): update auth secret env variable name

--- lib/auth.ts
+++ lib/auth.ts
@@ -76,5 +76,5 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.AUTH_SECRET,
 };
